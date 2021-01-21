5ddeccb801ea51a9a149705bf575227d1ef65168
pasor1 <pasor1@gmail.com>
2021-01-21 18:58:00 +0100

    homepage tiles for main navigation

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Homepage.scss';
+import Directory from '../../components/directory/Directory';
+
+const homePage = () => (
+  <div className="homepage">
+    <Directory/>
+  </div>
+);
+
+export default homePage;
